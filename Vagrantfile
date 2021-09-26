9b63a43008f03bad01276f4664af513ef620cd86
Wayne Wollesen <ewwollesen@dstsystems.com>
2021-09-26 13:52:48 -0500

    nginx proxy working with http

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,7 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "centos/7"
   config.vm.network "forwarded_port", guest:8065, host:8065
+  config.vm.network "forwarded_port", guest: 80, host: 80
   config.vm.hostname = 'mattermost'
 
   setup_script = File.read('vm_setup.sh')
--- vm_setup.sh
+++ vm_setup.sh
@@ -1,55 +1,58 @@
 #!/bin/bash
 
-# Read in secrets from file
+# Read in secrets and variables from file
 source /vagrant/.secrets
 
-# echo "Update host"
-# yum -y update
-
-echo "Install docker, wget, epel-release, jq, openldap-clients"
+echo "Installing docker, wget, epel-release, jq, openldap-clients, nginx..."
 yum -y install epel-release
-yum -y install docker wget jq openldap-clients
+yum -y install docker wget jq openldap-clients nginx
 
-echo "Starting Docker"
+echo "Starting Docker..."
 systemctl start docker
 
-echo "Install and set up Docker Compose"
+echo "Installing Docker Compose..."
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-echo "Run docker-compose"
+echo "Starting containers..."
 /usr/local/bin/docker-compose -f /vagrant/docker-compose.yml up -d
 
-echo "Download Mattermost version $MATTERMOST_VERSION"
+echo "Configuring nginx proxy..."
+setenforce 0 #selinux sucks
+cp /vagrant/mattermost.nginx.conf /etc/nginx/conf.d/mattermost
+ln -s /etc/nginx/conf.d/mattermost /etc/nginx/conf.d/default.conf
+systemctl restart nginx
+
+echo "Downloading Mattermost version $MATTERMOST_VERSION..."
 wget -q https://releases.mattermost.com/$MATTERMOST_VERSION/mattermost-$MATTERMOST_VERSION-linux-amd64.tar.gz
 
-echo "Install Mattermost"
+echo "Installing Mattermost..."
 tar -xzf mattermost-$MATTERMOST_VERSION-linux-amd64.tar.gz
 mv mattermost /opt
 mkdir /opt/mattermost/data
 
-echo "Create Mattermost config file"
+echo "Creating Mattermost config file..."
 mv /opt/mattermost/config/config.json /opt/mattermost/config/config.json.bak
 DATA_SOURCE="$MYSQL_USER:$MYSQL_PASSWORD@tcp(127.0.0.1:3306)/mattermost?charset=utf8mb4,utf8&writeTimeout=30s"
 jq '.SqlSettings.DataSource |= "'"$DATA_SOURCE"'"' /vagrant/config.json > /opt/mattermost/config/config.json
 
-echo "Create Mattermost user"
+echo "Create Mattermost sytem user and setting permissions..."
 useradd --system --user-group mattermost
 chown -R mattermost:mattermost /opt/mattermost
 chmod -R g+w /opt/mattermost
 
-echo "Install Mattermost systemd file"
+echo "Installing Mattermost systemd file..."
 cp /vagrant/mattermost.service /etc/systemd/system/mattermost.service
 chmod 644 /etc/systemd/system/mattermost.service
 systemctl daemon-reload
 
-echo "Create Mattermost admin user and team"
+echo "Creating Mattermost admin user and team..."
 # Should migrate to mmctl soon
 /opt/mattermost/bin/mattermost user create --email admin@planetexpress.com --username $MM_ADMIN --password $MM_ADMIN_PASSWORD --system_admin
 /opt/mattermost/bin/mattermost team create --name planet-express --display_name "Planet Express" --email "admin@planetexpress.com"
 /opt/mattermost/bin/mattermost team add planet-express admin@planetexpress.com
 
-echo "Starting Mattermost"
+echo "Starting Mattermost..."
 systemctl start mattermost.service
 
 # # Clean Up Files
@@ -63,3 +66,5 @@ echo $MYSQL_USER
 echo $MYSQL_PASSWORD
 echo $MATTERMOST_VERSION
 echo $DATA_SOURCE
+
+
